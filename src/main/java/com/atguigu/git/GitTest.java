e6e23d03f013174cfaf135598fa7278cd3473e0d
Huang <Huang@atguigu.com>
2023-03-06 21:14:32 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Huang
+ * @create 2023-03-06 21:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
